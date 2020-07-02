32ca2e6025463fd3a5148587745c43bf4ed7038c
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-07-02 21:17:23 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,16 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
-  end
+    doc = Nokogiri::HTML(open(https://learn-co-curriculum.github.io/student-scraper-test-page/)
+    doc.css(".student-card").each do|item|
+
+  
+
+
+
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
